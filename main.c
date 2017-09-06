677c8927345d784dc7f5b6998c58d6d29eee0cfb
Alex Taffe <alex@PowerEdge-R810.home>
2017-09-06 16:46:48 -0400

    Add main template and git ignore entry to avoid uploading binary

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]){
+	if(argc != 2){
+		printf("Correct number of arguments not provided. Usage: restrictions-crack <hash> <salt>\n");
+	}
+}
