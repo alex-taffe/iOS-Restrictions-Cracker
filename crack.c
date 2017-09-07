860fc2e83231a7af121b41a467c8515ff2cae78e
alex-taffe <alex.taffe@gmail.com>
2017-09-07 11:30:31 -0400

    Re indent code for visibility reasons

--- crack.c
+++ crack.c
@@ -21,13 +21,13 @@
 
 long cores;
 struct s_threadId {
-  pthread_mutex_t   mtx;    /* mutex & condition to allow main thread to
+    pthread_mutex_t   mtx;    /* mutex & condition to allow main thread to
                                wait for the new thread to  set its TID */
-  pthread_cond_t    cond;   /* '' */
-  pid_t             id;     /* to hold new thread's TID */
-  int               ready;  /* to indicate when 'id' is valid, in case
+    pthread_cond_t    cond;   /* '' */
+    pid_t             id;     /* to hold new thread's TID */
+    int               ready;  /* to indicate when 'id' is valid, in case
                                the condition wait gets interrupted */
-  int               numThread; /* used to hold the number of the thread */
+    int               numThread; /* used to hold the number of the thread */
 };
 
 const char *globalHash;
@@ -36,98 +36,98 @@ struct timeval start, end;
 
 
 void* crackSection(void* arg){
-	struct s_threadId *thId = arg;
-
-	/* Lock mutex... */
-	pthread_mutex_lock(&thId->mtx);
-
-	/* Get and save TID and ready flag.. */
-	thId->id = syscall(SYS_gettid);
-	thId->ready = 1;
-	/* ..and signal main thread that we're ready */
-	pthread_cond_signal(&thId->cond);
-
-	/* ..then unlock when we're done. */
-	pthread_mutex_unlock(&thId->mtx);
-
-
-	char *passcode = (char*)malloc(5 * sizeof(char));    
-
-	int i = thId->numThread;
-
-	int bottom = 0;
+    struct s_threadId *thId = arg;
+    
+    /* Lock mutex... */
+    pthread_mutex_lock(&thId->mtx);
+    
+    /* Get and save TID and ready flag.. */
+    thId->id = syscall(SYS_gettid);
+    thId->ready = 1;
+    /* ..and signal main thread that we're ready */
+    pthread_cond_signal(&thId->cond);
+    
+    /* ..then unlock when we're done. */
+    pthread_mutex_unlock(&thId->mtx);
+    
+    
+    char *passcode = (char*)malloc(5 * sizeof(char));
+    
+    int i = thId->numThread;
+    
+    int bottom = 0;
     if(i != 0)
         bottom = (ceil(9999.0 / cores) * (i));
     int top = (ceil(9999.0 / cores) * (i + 1)) - 1;
-
-	uint8_t out[20];
-
-	
-
-	for (int j = bottom; j <= top; j++) {
-
-		sprintf(passcode, "%04i", j);
-
-		fastpbkdf2_hmac_sha1(passcode, strlen(passcode), globalSalt, strlen(globalSalt), 1000, out, 20);
-
-		char *b64 = b64_encode(out, 20);
-
-		if(strcmp(globalHash, b64) == 0){
-			gettimeofday(&end, NULL);
-			double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
-			printf("The code is: %s\nCalculated in %.2f seconds\n", passcode, delta); 
-			exit(0);
-		}
+    
+    uint8_t out[20];
+    
+    
+    
+    for (int j = bottom; j <= top; j++) {
+        
+        sprintf(passcode, "%04i", j);
+        
+        fastpbkdf2_hmac_sha1(passcode, strlen(passcode), globalSalt, strlen(globalSalt), 1000, out, 20);
+        
+        char *b64 = b64_encode(out, 20);
+        
+        if(strcmp(globalHash, b64) == 0){
+            gettimeofday(&end, NULL);
+            double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+            printf("The code is: %s\nCalculated in %.2f seconds\n", passcode, delta);
+            exit(0);
+        }
     }
-
+    
 }
 
 void crackCode(const char *hash, const char *salt, char *code, char *error){
-	gettimeofday(&start, NULL);
-
-	globalHash = hash;
-	globalSalt = b64_decode(salt, 8);
-	
-	cores = sysconf(_SC_NPROCESSORS_ONLN);
-	printf("Cracking passcode with %li threads\n", cores);
-
-	pthread_t thread[cores]; /* reused for each thread, since they run 1 at a time */
-
-  	/* struct to pass back TID */
-  	struct s_threadId threadId;
-  	pthread_cond_init(&threadId.cond, NULL);  /* init condition */
-  	pthread_mutex_init(&threadId.mtx, NULL);  /* init mutex */
-
-
-	int err;
-
-	for(int i = 0; i < cores; i++){
-		/* lock mutex *before* creating the thread, to make the new thread
-		wait until we're ready before signaling us */
-		pthread_mutex_lock(&threadId.mtx);
-
-		/* clear ready flag before creating each thread */
-		threadId.ready = 0;
-		threadId.numThread = i;
-		/* create threads and pass address of struct as argument */
-		if (pthread_create(&thread[i], NULL, crackSection, &threadId)) {
-			printf("pthread error!\n");
-		} else {
-			/* Wait on the condition until the ready flag is set */
-			while (!threadId.ready) {
-				pthread_cond_wait(&threadId.cond, &threadId.mtx);
-			}
-			/* Now we have the TID... */
-			//printf("%d %d\n", i, threadId.id);
-			//printf("I just created thread %d\n", i);
-		}
-		/* ..and unlock the mutex when done. */
-		pthread_mutex_unlock(&threadId.mtx);
-	}
-	for(int i = 0; i < cores; i++){
-		pthread_join(thread[i], NULL);
-	}
-	gettimeofday(&end, NULL);
-	double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
-	printf("Code note found. Verify that you have entered the correct hash and salt\nTook %.2f seconds\n", delta);
+    gettimeofday(&start, NULL);
+    
+    globalHash = hash;
+    globalSalt = b64_decode(salt, 8);
+    
+    cores = sysconf(_SC_NPROCESSORS_ONLN);
+    printf("Cracking passcode with %li threads\n", cores);
+    
+    pthread_t thread[cores]; /* reused for each thread, since they run 1 at a time */
+    
+    /* struct to pass back TID */
+    struct s_threadId threadId;
+    pthread_cond_init(&threadId.cond, NULL);  /* init condition */
+    pthread_mutex_init(&threadId.mtx, NULL);  /* init mutex */
+    
+    
+    int err;
+    
+    for(int i = 0; i < cores; i++){
+        /* lock mutex *before* creating the thread, to make the new thread
+         wait until we're ready before signaling us */
+        pthread_mutex_lock(&threadId.mtx);
+        
+        /* clear ready flag before creating each thread */
+        threadId.ready = 0;
+        threadId.numThread = i;
+        /* create threads and pass address of struct as argument */
+        if (pthread_create(&thread[i], NULL, crackSection, &threadId)) {
+            printf("pthread error!\n");
+        } else {
+            /* Wait on the condition until the ready flag is set */
+            while (!threadId.ready) {
+                pthread_cond_wait(&threadId.cond, &threadId.mtx);
+            }
+            /* Now we have the TID... */
+            //printf("%d %d\n", i, threadId.id);
+            //printf("I just created thread %d\n", i);
+        }
+        /* ..and unlock the mutex when done. */
+        pthread_mutex_unlock(&threadId.mtx);
+    }
+    for(int i = 0; i < cores; i++){
+        pthread_join(thread[i], NULL);
+    }
+    gettimeofday(&end, NULL);
+    double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+    printf("Code note found. Verify that you have entered the correct hash and salt\nTook %.2f seconds\n", delta);
 }
