9c1dce2c8f9cbb42dafbba255d7cd4162a8e48ed
Alex Taffe <alex.taffe@gmail.com>
2018-01-20 16:06:33 -0500

    Add hash table implementation

--- crack.c
+++ crack.c
@@ -41,75 +41,75 @@ struct timeval start, end;
 
 void* crackSection(void* arg){
     struct s_threadId *thId = arg;
-    
+
     /* Lock mutex... */
     pthread_mutex_lock(&thId->mtx);
-    
+
     /* Get and save TID and ready flag.. */
     thId->id = syscall(SYS_gettid);
     thId->ready = 1;
     /* ..and signal main thread that we're ready */
     pthread_cond_signal(&thId->cond);
-    
+
     /* ..then unlock when we're done. */
     pthread_mutex_unlock(&thId->mtx);
-   
+
     int threadNumber = thId->numThread;
-    
+
     int bottom = 0;
     if(threadNumber != 0)
         bottom = (ceil(MAX_PASSCODE / cores) * (threadNumber));
     int top = (ceil(MAX_PASSCODE / cores) * (threadNumber + 1)) - 1;
-    
+
     uint8_t out[OUTPUT_LENGTH];
     char *b64;
     char *passcode = (char*)malloc(5 * sizeof(char));
     assert(passcode != NULL);
-    
+
     for (int i = bottom; i <= top; i++) {
-        
+
         sprintf(passcode, "%04i", i);
-        
+
         fastpbkdf2_hmac_sha1(passcode, 4, globalSalt, 4, 1000, out, OUTPUT_LENGTH);
-        
+
         b64 = b64_encode(out, OUTPUT_LENGTH);
-        
+
         if(strcmp(globalHash, b64) == 0){
             gettimeofday(&end, NULL);
             double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
             printf("The code is: %s\nCalculated in %.2f seconds\n", passcode, delta);
-		
+
             free(passcode);
             exit(0);
         }
     }
-    
+
 }
 
 void crackCode(const char *hash, const char *salt, char *code, char *error){
     gettimeofday(&start, NULL);
-    
+
     globalHash = hash;
     globalSalt = b64_decode(salt, 8);
-    
+
     cores = sysconf(_SC_NPROCESSORS_ONLN);
     printf("Cracking passcode with %li threads\n", cores);
-    
+
     pthread_t thread[cores]; /* reused for each thread, since they run 1 at a time */
-    
+
     /* struct to pass back TID */
     struct s_threadId threadId;
     pthread_cond_init(&threadId.cond, NULL);  /* init condition */
     pthread_mutex_init(&threadId.mtx, NULL);  /* init mutex */
-    
-    
+
+
     int err;
-    
+
     for(int i = 0; i < cores; i++){
         /* lock mutex *before* creating the thread, to make the new thread
          wait until we're ready before signaling us */
         pthread_mutex_lock(&threadId.mtx);
-        
+
         /* clear ready flag before creating each thread */
         threadId.ready = 0;
         threadId.numThread = i;
---
+++ table.h
@@ -0,0 +1,56 @@
+/// \file table.h
+/// \brief A generic hash table data structure.
+/// Author: Sean Strout (RIT CS)
+
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <stdbool.h>    // bool
+#include <stddef.h>     // size_t
+
+/// Initial capacity of table upon creation
+#define INITIAL_CAPACITY 16
+
+/// The load at which the table will rehash
+#define LOAD_THRESHOLD 0.75
+
+/// The table size will double upon each rehash
+#define RESIZE_FACTOR 2
+
+/// The "buckets" in the table
+typedef struct Entry_t {
+    void* key;     ///< key must be "hashable"
+    void* value;   ///< value can be any type
+} Entry;
+
+/// The hash table
+typedef struct Table_t {
+    Entry** table;    ///< table of Entry pointers
+    size_t size;      ///< number of entries in table
+    size_t capacity;  ///< total capacity of table
+    long (*hash)(void* key);  ///< hash function for key
+    bool (*equals)(void *key1, void* key2); ///< equals function for key comparison
+    void (*print)(void *key, void* value);  ///< print function for table dump debug
+    size_t collisions;      ///< number of collisions throughout life of hash table
+    size_t rehashes;        ///< number of rehashes throughout life of hash table
+} Table;
+
+Table* create(long (*hash)(void* key),
+              bool (*equals)(void* key1, void* key2),
+              void (*print)(void* key1, void* key2));
+
+void destroy(Table* t);
+
+void dump(Table* t, bool full);
+
+void* get(Table* t, void* key);
+
+bool has(Table* t, void* key);
+
+void** keys(Table* t);
+
+void* put(Table* t, void* key, void* value);
+
+void** values(Table* t);
+
+#endif // TABLE_H
